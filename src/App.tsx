0250dad9fe3d7f364b93be8967242ee39fb15198
August White <blackhuman01@gmail.com>
2024-03-19 21:42:59 +0800

    完成save/restore功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import { useState } from 'react'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css'
-import ReactFlow, { Controls, Handle, NodeToolbar, Position, ReactFlowProvider, addEdge, applyEdgeChanges, applyNodeChanges, useReactFlow, useStoreApi, useUpdateNodeInternals, useViewport } from 'reactflow';
-import type {Connection, Dimensions, Edge, EdgeChange, HandleProps, Node, NodeChange, NodeProps, NodeTypes, OnConnect, OnConnectEnd, OnConnectStart, OnConnectStartParams, OnEdgesChange, OnNodesChange, ReactFlowInstance, Rect, XYPosition} from 'reactflow'
+import ReactFlow, { Controls, Handle, NodeToolbar, Panel, Position, ReactFlowProvider, applyEdgeChanges, applyNodeChanges, useReactFlow, useStore as useStoreReactFlow, useStoreApi, useUpdateNodeInternals, useViewport, useNodesState, useEdgesState } from 'reactflow';
+import type {ConnectingHandle, Connection, Dimensions, Edge, EdgeChange, HandleProps, Node, NodeChange, NodeProps, NodeTypes, OnConnect, OnConnectEnd, OnConnectStart, OnConnectStartParams, OnEdgesChange, OnNodesChange, ReactFlowInstance, Rect, XYPosition} from 'reactflow'
 import 'reactflow/dist/style.css';
 import { Button } from '@/components/ui/button';
 import {create} from 'zustand'
+import { UpdateIcon } from '@radix-ui/react-icons';
 
 interface GridLine {
   xList: number[]
@@ -121,126 +122,162 @@ type GridNodeData = {
   
 }
 
-type HandlePropsEx = HandleProps & {
-  isInitialHandle: boolean,
-}
-
-type HandleMap = Record<string, HandlePropsEx[]>
+type HandleMap = Record<string, HandleProps[]>
 
 type AppStore = {
-  nodes: Node[];
-  edges: Edge[];
-  onNodesChange: OnNodesChange;
-  onEdgesChange: OnEdgesChange;
-  onConnect: OnConnect;
-  setNodes: (nodes: Node[]) => void;
-  setEdges: (edges: Edge[]) => void;
-  setNode: (nodeId: string, c: (node: Node) => void) => void
-  addNode: (node: Omit<Node, 'id'>) => string
-  getNode: (nodeId: string) => Node | null
-  handleMap: HandleMap
-  getHandles(nodeId: string): HandlePropsEx[]
-  addHandle(nodeId: string, type: 'source' | 'target', isInitialHandle: boolean): string
-  connectNodes(sourceNodeId: string, targetNodeId: string): void
   isConnecting: boolean
   setConnecting(isConnecting: boolean): void
-  reset(): void
+  handleMap: HandleMap
+  clearHandle(): void
+  getHandles(nodeId: string): HandleProps[]
+  addHandle(handle: ConnectingHandle): string
 }
 
-const useStore = create<AppStore>((set, get) => ({
-  nodes: [],
-  edges: [],
-  isConnecting: false,
-  setConnecting(isConnecting) {
-      set({isConnecting})
-  },
-  onNodesChange: (changes: NodeChange[]) => {
-    set({
-      nodes: applyNodeChanges(changes, get().nodes),
-    });
-  },
-  onEdgesChange: (changes: EdgeChange[]) => {
-    set({
-      edges: applyEdgeChanges(changes, get().edges),
-    });
-  },
-  onConnect: (connection: Connection) => {
-    set({
-      edges: addEdge(connection, get().edges),
-    });
-  },
-  setNodes: (nodes: Node[]) => {
-    set({ nodes });
-  },
-  setNode: (nodeId, c) => {
-    const node = get().nodes.filter(n => n.id === nodeId)[0]
-    if (node === undefined) return
-    c(node)
-    set({ nodes: [...get().nodes, node]})
-  },
-  addNode: (node) => {
-    const newNode = {
-      ...node,
-      type: GRID_NODE_TYPE_NAME,
-      id: String(get().nodes.length + 1)
-    }
-    get().addHandle(newNode.id, 'source', true)
-    get().addHandle(newNode.id, 'target', true)
-    set({ nodes: [...get().nodes, newNode] })
-    return newNode.id
-  },
-  getNode: (nodeId: string) => {
-    return get().nodes.filter(n => n.id === nodeId)[0]
-  },
-  setEdges: (edges: Edge[]) => {
-    set({ edges });
-  },
-  handleMap: {},
-  getHandles(nodeId) {
-    return get().handleMap[nodeId] ?? []
-  },
-  addHandle: (nodeId, type, isInitialHandle): string => {
-    let handleId = ''
-    set((state): Pick<AppStore, 'handleMap'> => {
-      const handles = state.handleMap[nodeId] ?? []
-      handleId = `${type}_${handles.length+1}_${isInitialHandle}`
-      const newHandle: HandlePropsEx = {
-        id: handleId,
-        type,
-        position: type === 'source' ? Position.Right : Position.Left,
-        isConnectable: true,
-        isConnectableStart: type==='source',
-        isConnectableEnd: type==='target',
-        isInitialHandle: isInitialHandle,
+type ConnectNodes = (source: ConnectingHandle, target: ConnectingHandle) => void
+
+const useNodesStateEx: typeof useNodesState = (initialItems) => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialItems)
+  const updateNodeInternals = useUpdateNodeInternals()
+  return [
+    nodes,
+    (nodes) => {
+      setNodes(nodes)
+      if (typeof nodes === 'function') return
+      updateNodeInternals(nodes.map(node => node.id))
+    },
+    onNodesChange
+  ]
+}
+
+const useEdgesStateEx: typeof useEdgesState = (initialItems) => {
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialItems)
+  const {addEdge} = useReactFlowEx()
+  const setEdgesEx: typeof setEdges = (edges) => {
+    if (typeof edges === 'function') {
+      setEdges(edges)
+      return
+    } else {
+      for (const edge of edges) {
+        addEdge({
+          nodeId: edge.source,
+          handleId: edge.sourceHandle,
+          type: 'source',
+        }, {
+          nodeId: edge.target,
+          handleId: edge.targetHandle,
+          type: 'target',
+        })
       }
-      return {handleMap: {...state.handleMap, [nodeId]: [...handles, newHandle]}}
+    }
+  }
+  return [
+    edges, 
+    setEdgesEx, 
+    onEdgesChange
+  ]
+}
+
+type ReactFlowInstanceEx = ReactFlowInstance & {
+  addNode(node: Omit<Node, 'id'>): string
+  updateNode(nodeId: string, modify: (node: Node) => void): void
+  addEdge(source: ConnectingHandle, target: ConnectingHandle): void
+}
+
+function useReactFlowEx(): ReactFlowInstanceEx {
+  const reactFlowInstance = useReactFlow()
+  const {getNodes, addNodes, getNode, setNodes, setEdges, getEdges} = reactFlowInstance
+  const addHandle = useStoreLocal(state => state.addHandle)
+  const updateNodeInternals = useUpdateNodeInternals()
+  const {addEdges} = useReactFlow()
+  const addNode: ReactFlowInstanceEx['addNode'] = (node) => {
+    const nodeId = `${getNodes().length + 1}`
+    addNodes({
+      id: nodeId,
+      type: GRID_NODE_TYPE_NAME,
+      ...node
     })
-    return handleId
-  },
-  connectNodes: (sourceNodeId: string, targetNodeId: string) => {
-    const addHandle = get().addHandle
-    const sourceHandleId = addHandle(sourceNodeId, 'source', false)
-    const targetHandleId = addHandle(targetNodeId, 'target', false)
+    return nodeId
+  }
+  const updateNode: ReactFlowInstanceEx['updateNode'] = (nodeId, modify) => {
+    setNodes(nodes => nodes.map(n => {
+      if (n.id === nodeId) {
+        modify(n)
+        return n
+      } else {
+        return n
+      }
+    }))
+  }
+  const addEdge: ReactFlowInstanceEx['addEdge'] = (source, target) => {
+    const sourceHandleId = addHandle(source)
+    const targetHandleId = addHandle(target)
+    updateNodeInternals([source.nodeId, target.nodeId])
     const edge: Edge = {
-      id: `${sourceNodeId}_${sourceHandleId}:${targetNodeId}_${targetHandleId}`,
-      source: sourceNodeId,
+      id: `${source.nodeId}_${sourceHandleId}:${target.nodeId}_${targetHandleId}`,
+      source: source.nodeId,
       sourceHandle: sourceHandleId,
-      target: targetNodeId,
+      target: target.nodeId,
       targetHandle: targetHandleId,
     }
-    get().setEdges([...get().edges, edge])
-  },
-  reset() {
-    get().setNodes([])
-    get().setEdges([])
-    get().handleMap = {}
-  },
-}))
+    addEdges(edge)
+  }
+  return {
+    ...reactFlowInstance,
+    addNode,
+    updateNode,
+    addEdge,
+  }
+}
+
+function useOperationReset(): () => void {
+  const {setNodes, setEdges} = useReactFlow()
+  const clearHandle = useStoreLocal(state => state.clearHandle)
+  return () => {
+    setNodes([])
+    setEdges([])
+    clearHandle()
+  }
+}
+
+const useStoreLocal = create<AppStore>((set, get) => {
+  return {
+    isConnecting: false,
+    setConnecting(isConnecting) {
+        set({isConnecting})
+    },
+    handleMap: {},
+    clearHandle() {
+        set({handleMap: {}})
+    },
+    getHandles(nodeId) {
+      return get().handleMap[nodeId] ?? []
+    },
+    addHandle: (handle): string => {
+      const handles = get().handleMap[handle.nodeId] ?? []
+      const type = handle.type
+      if (handle.handleId === undefined) {
+        handle.handleId = `${type}_${handles.length+1}`
+      }
+      set((state): Pick<AppStore, 'handleMap'> => {
+        const newHandle: HandleProps = {
+          id: handle.handleId!,
+          type,
+          position: type === 'source' ? Position.Right : Position.Left,
+          isConnectable: true,
+          isConnectableStart: type==='source',
+          isConnectableEnd: type==='target',
+        }
+        return {handleMap: {...state.handleMap, [handle.nodeId]: [...handles, newHandle]}}
+      })
+      return handle.handleId!
+    },
+  }
+})
 
 function GridNode({sourcePosition, isConnectable, data, id: nodeId}: NodeProps<GridNodeData>) {
   const [toolbarVisible, setToolbarVisible] = useState(false)
-  const handles = useStore(state => state.getHandles(nodeId))
-  const isConnecting = useStore(state => state.isConnecting)
+  const handles = useStoreLocal(state => state.getHandles(nodeId))
+  const isConnecting = useStoreLocal(state => state.isConnecting)
 
   function hideToolbarDelay() {
     setTimeout(() => setToolbarVisible(false), 500)
@@ -260,30 +297,38 @@ function GridNode({sourcePosition, isConnectable, data, id: nodeId}: NodeProps<G
         >
         <p>{data.column}</p>
         <p>{nodeId}</p>
+        <Handle
+          id='1'
+          type='target'
+          position={Position.Left}
+          isConnectableStart={false}
+          isConnectableEnd={true}
+          isConnectable={true}
+          style={{pointerEvents: `${isConnecting ? 'auto' : 'none'}`}}
+          className='target-handle' 
+          />
+        <Handle
+          id='2'
+          type='source'
+          position={Position.Right}
+          isConnectableStart={true}
+          isConnectableEnd={false}
+          isConnectable={true}
+          className='source-handle' 
+          />
         {
           handles.map((props, _, array) => {
-            const initialSourceHandleClass = props.isInitialHandle && props.type === 'source' ? 'source-handle' : ''
-            const initialTargetHandleClass = props.isInitialHandle && props.type === 'target' ? 'target-handle' : ''
             const isConnectableStart = props.type === 'source'
-            const isConnectableEnd = props.type === 'target'
+            // const isConnectableEnd = props.type === 'target'
             const style: React.CSSProperties = {}
-            if (!props.isInitialHandle) {
-              if (isConnectableStart) {
-                const filteredArray = array.filter(v => !v.isInitialHandle && v.isConnectableStart)
-                const index = filteredArray.findIndex(v => v.id === props.id)
-                style.top = 50 / (filteredArray.length + 1) * (index + 1)
-              } else {
-                const filteredArray = array.filter(v => !v.isInitialHandle && !v.isConnectableStart)
-                const index = filteredArray.findIndex(v => v.id === props.id)
-                style.top = 50 / (filteredArray.length + 1) * (index + 1)
-              }
+            if (isConnectableStart) {
+              const filteredArray = array.filter(v => v.isConnectableStart)
+              const index = filteredArray.findIndex(v => v.id === props.id)
+              style.top = 50 / (filteredArray.length + 1) * (index + 1)
             } else {
-              if (isConnectableEnd) {
-                // style.zIndex = isConnecting ? 1000 : 0
-                style.pointerEvents = isConnecting ? 'auto' : 'none'
-                // style.opacity = isConnecting ? 1 : 1
-                // console.log('style', style)
-              }
+              const filteredArray = array.filter(v => !v.isConnectableStart)
+              const index = filteredArray.findIndex(v => v.id === props.id)
+              style.top = 50 / (filteredArray.length + 1) * (index + 1)
             }
             
             return (
@@ -296,7 +341,6 @@ function GridNode({sourcePosition, isConnectable, data, id: nodeId}: NodeProps<G
                 isConnectableEnd={!isConnectableStart}
                 isConnectable={true}
                 style={style}
-                className={`${initialSourceHandleClass} ${initialTargetHandleClass}`} 
                 />
             )
           })
@@ -370,27 +414,18 @@ function Main() {
     [GRID_NODE_TYPE_NAME]: GridNode
   }), [])
 
-  const {
-    nodes, setNode, addNode, setNodes, onNodesChange,
-    edges, onEdgesChange,
-    connectNodes,
-    reset, setConnecting,
-  } = useStore((state) => ({
-    nodes: state.nodes,
-    edges: state.edges,
-    setNodes: state.setNodes,
-    setNode: state.setNode,
-    addNode: state.addNode,
-    onNodesChange: state.onNodesChange,
-    onEdgesChange: state.onEdgesChange,
-    onConnect: state.onConnect,
-    connectNodes: state.connectNodes,
-    reset: state.reset,
-    setConnecting: state.setConnecting,
-  }))
+  const [nodes, setNodes, onNodesChange] = useNodesStateEx([{
+    id: '1',
+    data: { column: 1, row: 1 },
+    position: {x: 0, y: 0},
+    type: GRID_NODE_TYPE_NAME,
+  }])
+  const [edges, setEdges, onEdgesChange] = useEdgesStateEx([])
+  // const connectNodes = useOperationConnectNodes()
+  const reset = useOperationReset()
+  const setConnecting = useStoreLocal(state => state.setConnecting)
 
-  const updateNodeInternals = useUpdateNodeInternals()
-  const { fitView, screenToFlowPosition } = useReactFlow()
+  const { fitView, screenToFlowPosition, toObject, addNode, updateNode, addEdge } = useReactFlowEx()
   const store = useStoreApi()
   const {width, height} = store.getState()
   const { x, y, zoom } = useViewport()
@@ -398,15 +433,6 @@ function Main() {
   const gridLine = useMemo(() => layoutManager.getGridLinesInViewPort({x: 0, y: 0, width: 100, height: 100}), [])
   const [currentCell, setCurrentCell] = useState<Cell|null>(null)
   const [currentTargetCell, setCurrentTargetCell] = useState<Cell|null>(null)
-
-  useEffect(() => {
-    const nodeId = addNode({
-      data: { column: 1, row: 1 },
-      position: {x: 0, y: 0},
-    })
-    updateNodeInternals(nodeId)
-    return () => reset()
-  }, [])
 
   useEffect(() => {
 
@@ -418,7 +444,6 @@ function Main() {
         addNode({
           position: {x: cell.rect.x, y: cell.rect.y},
           data: { column: cell.column, row: cell.row },
-          type: GRID_NODE_TYPE_NAME,
         })
       }
     }
@@ -426,17 +451,7 @@ function Main() {
     return () => {
       document.removeEventListener('dblclick', listener)
     }
-  }, [zoom])
-
-  const onInit = useCallback((_: ReactFlowInstance<NodeData, EdgeData>) => {
-    // const nodeId = addNode({
-    //   data: { column: 1, row: 1 },
-    //   position: {x: 0, y: 0},
-    // })
-    // updateNodeInternals(nodeId)
-
-    console.log('onInit')
-  }, [])
+  }, [zoom, screenToFlowPosition, layoutManager, addNode])
 
   const onNodeDrag = useCallback((event: React.MouseEvent, node: Node, nodes: Node[]) => {
     const position = screenToFlowPosition({x: event.clientX, y: event.clientY})
@@ -445,14 +460,14 @@ function Main() {
       node.data.column = cell.column
       setCurrentCell(cell)
     }
-  }, [x, y, zoom])
+  }, [layoutManager, screenToFlowPosition])
 
   const onNodeDragStop = useCallback((event: React.MouseEvent, currentNode: Node) => {
     if (currentCell !== null) {
-      setNode(currentNode.id, node => node.position = currentCell.rect as XYPosition)
+      updateNode(currentNode.id, node => node.position = currentCell.rect as XYPosition)
     }
     setCurrentCell(null)
-  }, [currentCell, setNode, nodes])
+  }, [currentCell, updateNode])
 
   interface Refs {
     hasConnected: boolean
@@ -478,34 +493,26 @@ function Main() {
     const position = screenToFlowPosition({x: event.clientX, y: event.clientY})
     const cell = layoutManager.findCellAt(position)
     if (cell === null) return
-    const newNodeId = String(nodes.length + 1) 
-    const newNodeDef: Node<GridNodeData> = {
-      id: newNodeId,
+    const newNodeId = addNode({
       position: {x: cell.rect.x, y: cell.rect.y},
       data: { column: 1, row: 1 },
-      type: GRID_NODE_TYPE_NAME,
-    }
-    addNode(newNodeDef)
+    })
 
-    const originalNodeId = isConnectStart.nodeId!
-
-    connectNodes(originalNodeId, newNodeId)
-    updateNodeInternals([originalNodeId, newNodeId])
-  }, [nodes, setNodes, store, ref])
+    addEdge(
+      { nodeId: isConnectStart.nodeId!, type: 'source' }, 
+      { nodeId: newNodeId, type: 'target' }
+    )
+  }, [setConnecting, screenToFlowPosition, layoutManager, addNode, addEdge])
 
   const onConnect = useCallback((params: Connection) => {
     const { source, target } = params
     if (source === null || target === null) return
     ref.current.hasConnected = true
-    connectNodes(source, target)
-    updateNodeInternals([source, target])
-  }, [connectNodes, updateNodeInternals])
+    addEdge({nodeId: source, type: 'source'}, {nodeId: target, type: 'target'})
+  }, [addEdge])
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
-      // console.log('mousemove', event.clientX, event.clientY)
-      // const elems = document.elementsFromPoint(event.clientX, event.clientY);
-      // console.log('elems', elems)
       if (isConnectStartRef.current === undefined) return
       const position = screenToFlowPosition({x: event.clientX, y: event.clientY})
       const cell = layoutManager.findCellAt(position)
@@ -517,12 +524,28 @@ function Main() {
     return (() => document.removeEventListener('mousemove', listener))
   }, [layoutManager, screenToFlowPosition])
 
-  // const storeApi = useStoreApi()
-  // useEffect(() => {
-  //   storeApi.subscribe((state) => {
-  //     console.log('state', state)
-  //   })
-  // }, [storeApi])
+  const flowKey = 'reactflow'
+
+  const onSave = useCallback(() => {
+    const flow = toObject();
+    localStorage.setItem(flowKey, JSON.stringify(flow));
+  }, [toObject]);
+
+  const onRestore = useCallback(() => {
+    const restoreFlow = async () => {
+      const flow = JSON.parse(localStorage.getItem(flowKey)!);
+
+      if (flow) {
+        // const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        reset()
+        setNodes(flow.nodes || [])
+        setEdges(flow.edges || [])
+        // setViewport({ x, y, zoom });
+      }
+    };
+
+    restoreFlow();
+  }, [setNodes, setEdges]);
 
   return (
     <ReactFlow
@@ -541,7 +564,6 @@ function Main() {
       isValidConnection={() => {
         return true
       }}
-      onInit={onInit}
       zoomOnDoubleClick={false}
     >
       <BackgroundGrid 
@@ -552,6 +574,10 @@ function Main() {
         currentCell={currentCell}
         currentTargetCell={currentTargetCell} />
       <Controls />
+      <Panel position="top-right">
+        <Button onClick={onSave}>save</Button>
+        <Button onClick={onRestore}>restore</Button>
+      </Panel>
     </ReactFlow>
   )
 }
