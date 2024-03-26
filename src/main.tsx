4dd558e6242c8a51d7f8400773015f561d11e217
August White <blackhuman01@gmail.com>
2024-03-26 15:11:06 +0800

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ReactFlowProvider } from 'reactflow'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className='h-screen w-screen'>
       <ReactFlowProvider>
         <App/>
+        <Analytics/>
       </ReactFlowProvider>
     </div>
   </React.StrictMode>
